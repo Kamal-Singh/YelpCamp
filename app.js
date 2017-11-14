3783e1859236e88ab39c70634fd48deb0bf28c5d
Kamal Singh <ksinghyadav@ymail.com>
2017-11-14 18:26:30 +0530

    Added Mongoose Package to the project and changed routes accoridng to it

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelp_camp', {useMongoClient: true});
+mongoose.Promise = global.Promise;
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+//Create a schema for campground
+var schema = mongoose.Schema({
+    name: String,
+    image: String
+});
+//create a model of the schema
+var campgrounds = mongoose.model('campground', schema);
+
+// campground.create({
+//     name: 'River Way Ranch Camp', 
+//     image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg'
+// }, function(err, campground){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(campground);
+//     }
+// });
+/*
 var campgrounds = [
     { name: 'Tripp Lake Camp', image: 'https://cdn.pixabay.com/photo/2017/10/28/23/18/indians-2898463__340.jpg'},
     { name: 'River Way Ranch Camp', image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg'},
@@ -13,14 +37,20 @@ var campgrounds = [
     { name: 'Tripp Lake Camp', image: 'https://cdn.pixabay.com/photo/2017/10/28/23/18/indians-2898463__340.jpg'},
     { name: 'River Way Ranch Camp', image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg'},
     { name: 'Raquette Lake Camps', image: 'https://cdn.pixabay.com/photo/2017/08/29/04/16/site-2692058__340.jpg'}
-];
+];*/
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res){
-     res.render('camps', {campgrounds: campgrounds});
+    campgrounds.find({}, function(err, campgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('index', {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req, res){
@@ -28,13 +58,19 @@ app.post('/campgrounds', function(req, res){
         name: req.body.name,
         image: req.body.image
     };
-    campgrounds.push(new_camp);
-    res.redirect('/campgrounds');
+    campgrounds.create(new_camp, function(err, campground){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
 });
 
 app.get('/campgrounds/new', function(req, res){
-    res.render('new_campground');
+    res.render('new');
 });
+
 app.listen(3000, function(){
     console.log('The Yelp Camp is running on http://localhost:3000');
 });
